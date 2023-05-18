edc9898b8632886d92a43c711f0e954ed553bbec
Edward Simpson <76537760+esimpson07@users.noreply.github.com>
2023-05-18 14:46:30 -0400

    Update p5.js

--- p5.js
+++ p5.js
@@ -2,68 +2,67 @@ var angle = 3.141592 / 4;//45 degrees
 var firePower = 10;//10 meters / s
 var gravity = -9.8;//-9.8 meters / s^2
 var metersToPX = 30; //30 pixels in a simulated meter
-var xDist = 0;
-var yDist = 0;
-var airTime = 0;
-var initMillis = 0;
-var xCom = 0;
-var yCom = 0;
-var elasticity = 0.8;
-var yMult = 1;
-var startX = 0;
-var dT = 100; //milliseconds
-const cannonPos = [];
+var xDist = 0; //the distance the current bounce will travel
+var yDist = 0; //the height the current bounce will travel
+var airTime = 0; //the air time of the current bounce in s
+var initMillis = 0; // when the current bounce started
+var xCom = 0; //x component of fire vector
+var yCom = 0; //y component of fire vector
+var elasticity = 0.8; // ratio of bounce vel / prev bounce vel
+var startX = 0; // the starting x position of the current bounce
+const cannonPos = []; //coordinates of cannon position
 
 function setup() {
-  createCanvas(400, 400);
-  frameRate(15);
-  background(240);
-  cannonPos.push(10,390);
+  createCanvas(400, 400); //400 x 400 window -> 30 PX = 1 meter
+  frameRate(30); //X FPS -> X dots per second
+  background(240); // set background to (240,240,240)
+  cannonPos.push(10,390); //set cannon position to 10, 390 -> bottom left of the screen
 }
 
 function draw() {
-  fill(255, 60, 100);
+    angle = abs(atan((mouseY - cannonPos[1]) / (mouseX - cannonPos[0]))); // give the cannon an angle
+  fill(255, 60, 100); // sets the circle color to red
   circle(cannonPos[0] + startX + (metersToPX * xDist * (millis() - initMillis) / (airTime * 1000)),(400 - (metersToPX * getHeight(initMillis, millis()))) - (400 - cannonPos[1]),10);
-  fill(0, 0, 0);
-  circle(cannonPos[0],cannonPos[1],20);
+  fill(0, 0, 0); // draws a dot depending on how far the shot is
+  circle(cannonPos[0],cannonPos[1],20); // draws cannon
 }
 
 function keyPressed(){
-  if (key == ' '){
-    background(240);
-    fill(255, 60, 100);
-    startX = 0;
-    angle = abs(atan((mouseY - cannonPos[1]) / (mouseX - cannonPos[0])));
-    fire(angle);
+  if (key == ' '){ // if space pressed
+    background(240); // clear canvas
+    fill(255, 60, 100); // set the draw color
+    startX = 0; // reset all the instance variables
+    fire(angle); // run the math to fire a shot
   } else if(key == 'ArrowUp') {
-    firePower ++;
+    firePower ++; // increases firepower by 1 m / s each time the up arrow is pressed 
   } else if(key == 'ArrowDown') {
-    firePower --;
+    firePower --; // decreases firepower by 1 m / s each time the down arrow is pressed
   }
 }
 
 function fire(a, p) {
-  var pos = 0;
   xCom = firePower * cos(a);
   yCom = firePower * sin(a);
+  psuedoFire();
+  fill(0,0,0);
+  circle(cannonPos[0] + 10 * cos(angle), cannonPos[1] - 10 * sin(angle), 20); // draws end of cannon pointing in fire direction
+}
+
+function psuedoFire() { // does the math to find time that the current bounce is in the air, the maximum height of the vertex, and the distance traveled in the x direction
   airTime = - 2 * abs(yCom) / gravity
   xDist = xCom * airTime;
   yDist = -(yCom * yCom) / (2 * gravity);
   initMillis = millis();
 }
 
-function getHeight(start, current) {
+function getHeight(start, current) { //returns the height of the ball at a given instant, based on when psuedoFire() was last called. If the height is less than 0, the function refires the ball but with less velocity.
   var x = (current - start) / 1000;
-  var a = gravity;
-  var vInitial = yCom;
-  var y = ((x * x) * a / 2) + (vInitial * x);
+  var y = ((x * x) * gravity / 2) + (yCom * x);
   if(y < 0) {
-    startX = metersToPX * xDist + startX;
+    startX += metersToPX * xDist;
     yCom *= elasticity;
-    airTime = - 2 * abs(yCom) / gravity
-    xDist = xCom * airTime;
-    yDist = -(yCom * yCom) / (2 * gravity);
-    initMillis = millis();
+    psuedoFire();
+    var y = ((x * x) * gravity / 2) + (yCom * x);
   }
   return y;
 }
